b100d8997cf81ff559f4a5a6b0f0cdc4fc5ee7e5
WesleyAraujoFernandes <wesley@viaapia.com.br>
2025-05-20 11:42:16 -0300

    commit inicial

---
+++ src/main/java/br/com/viaapia/analystnotebook/AnalystnotebookApplication.java
@@ -0,0 +1,13 @@
+package br.com.viaapia.analystnotebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalystnotebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalystnotebookApplication.class, args);
+	}
+
+}
